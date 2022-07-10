30c14b221705c67f179c1fc2dc38435f7834be75
pmagalhaes2 <paath.magalhaes2@gmail.com>
2022-07-10 16:51:33 -0300

    inserindo projeto

---
+++ src/components/Menu/Menu.jsx
@@ -0,0 +1,26 @@
+import { Link } from "react-router-dom";
+import "./menu.css";
+
+function Menu() {
+  return (
+    <ul className="menu">
+      <li className="item">
+        <Link className="link" to="/">
+          Sobre
+        </Link>
+      </li>
+      <li className="item">
+        <Link className="link" to="/portfolio">
+          Porfólio
+        </Link>
+      </li>
+      <li className="item">
+        <Link className="link" to="/comentarios">
+          Comentários
+        </Link>
+      </li>
+    </ul>
+  );
+}
+
+export default Menu;
---
+++ src/pages/Portfolio/Portfolio.jsx
@@ -0,0 +1,12 @@
+import Header from "../../components/Header/Header";
+import image from "../../assets/projects-icon.svg";
+
+function Portfolio() {
+  return (
+    <Header image={image} description="imagem mulher criando projeto">
+      Meus projetos
+    </Header>
+  );
+}
+
+export default Portfolio;
